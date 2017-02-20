7c5666ed3eb353b172503c250d0674b4dac414f8
gureks <gurek15033@iiitd.ac.in>
2017-02-20 22:51:18 +0530

    fixed views
    Signed-off-by: gureks <gurek15033@iiitd.ac.in>

--- iiitdmun/mun/urls.py
+++ iiitdmun/mun/urls.py
@@ -3,9 +3,9 @@ from . import views
 urlpatterns = [
     url(r'^$',views.index,name='index'),
     url(r'^register/$',views.register,name='register'),
-    url(r'^collegedelegation/$',views.collegedelegation,name='register'),
+    url(r'^delegation/$',views.collegedelegation,name='register'),
     url(r'^ip/$',views.ip,name='ip'),
-    url(r'^individualdelegation/$',views.individualdelegation,name='individualdelegation'),
+    url(r'^delegate/$',views.individualdelegation,name='individualdelegation'),
     url(r'^form_submitted/$',views.ipformsubmit,name='form_submitted'),
     url(r'^form_submitted1/$',views.indisubmit,name='form_submitted1'),
     url(r'^form_submitted2/$',views.collsubmit,name='form_submitted2'),
