71c4187a93bca957a7608aab2163b5fa929bb2ea
snehalgupta <snehal16201@iiitd.ac.in>
2017-03-16 17:53:43 +0530

    Added link for team.html

--- iiitdmun/mun/urls.py
+++ iiitdmun/mun/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     url(r'^form_submitted1/$',views.indisubmit,name='form_submitted1'),
     url(r'^form_submitted2/$',views.collsubmit,name='form_submitted2'),
     url(r'^form_submitted3/$',views.consubmit,name='form_submitted3'),
+    url(r'^team2/$',views.team2,name='team2'),
     url(r'^success/$',views.success,name='success'),
    
     ]
